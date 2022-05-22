1fcf59db976feab5d7a7d41df0977ee6533d872e
Igor Burenkov <49118930+EveryBarry@users.noreply.github.com>
2022-05-22 14:47:12 +0300

    feat: add path in 1 task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,20 +1,23 @@
-let fs = require('fs');
+const fs = require('fs');
+const path = require('path');
 
-let stream = new fs.ReadStream('01-read-file/text.txt', {encoding: 'utf-8'});
+const text = path.join(__dirname, 'text.txt');
+
+let stream = new fs.ReadStream(text, { encoding: 'utf-8' });
 
 stream.on('readable', function () {
   let data = stream.read();
-  if(data != null) console.log(data);
+  if (data != null) console.log(data);
 });
 
 // stream.on('end', function () {
 //   console.log('THE END');
 // });
 
-stream.on('error', function(err){
-  if(err.code == 'ENOENT'){
+stream.on('error', function (err) {
+  if (err.code == 'ENOENT') {
     console.log('Файл не найден');
-  }else{
+  } else {
     console.error(err);
   }
 });
