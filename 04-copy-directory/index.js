8d4176b176378240f3530e070530d2f71a98168d
Igor Burenkov <49118930+EveryBarry@users.noreply.github.com>
2022-05-23 18:00:34 +0300

    feat: complete task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,47 @@
+/**
+* 1. Импорт всех требуемых модулей
+* 2. Создание папки **files-copy** в случае если она ещё не существует
+* 3. Чтение содержимого папки **files**
+* 4. Копирование файлов из папки **files** в папку **files-copy**
 
+    start -- node 04-copy-directory
+ */
+
+const fs = require('fs');
+const path = require('path');
+const folderPath = path.join(__dirname, 'files');
+const copyPath = path.join(__dirname, 'files-copy');
+
+
+(function copyFiles() {
+  fs.mkdir(copyPath, 
+    { recursive: true }, 
+    err => {
+      if (err) console.log(err);
+    });
+      
+  fs.readdir(copyPath, 
+    (err, 
+      data) => {
+      if (err) console.log(err);
+
+      data.forEach((elem) => {
+        fs.unlink(`${copyPath}/${elem}`, 
+          err => {
+            if (err) console.log(err);
+          });
+      });
+    });
+      
+  fs.readdir(folderPath, 
+    (err, data) => {
+      if (err) console.log(err);
+      data.forEach((elem) => {
+        fs.copyFile(`${folderPath}/${elem}`, `${copyPath}/${elem}`, (err) => {
+          if (err) console.log(err);
+          else console.log('copy file: ' + elem + ' - successful');
+        });
+      });
+    });
+})();
+  
